a8dd2645e72173d9a46b1dbc35956d015016854c
Maudi Rahul <maudirahul43489@gmail.com>
2026-03-16 02:45:00 +0530

    change in nodemailer configuration

--- backend/src/utils/sendEmail.js
+++ backend/src/utils/sendEmail.js
@@ -1,24 +1,26 @@
 const nodemailer = require("nodemailer");
 
 const sendEmail = async (options) => {
-  // 1. Create a transporter (the service that actually sends the email)
   const transporter = nodemailer.createTransport({
-    service: "Gmail",
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true, 
     auth: {
       user: process.env.EMAIL_USERNAME,
       pass: process.env.EMAIL_PASSWORD,
     },
+    tls: {
+      rejectUnauthorized: false
+    }
   });
 
-  // 2. Define the email options
   const mailOptions = {
     from: `ArchLens Support <${process.env.EMAIL_USERNAME}>`,
     to: options.email,
     subject: options.subject,
-    html: options.html, 
+    html: options.html,
   };
 
-  // 3. Send the email
   await transporter.sendMail(mailOptions);
 };
 
