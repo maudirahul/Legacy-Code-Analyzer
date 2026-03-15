6afd3a3ea4b9caf45614a6d7019feb6466133b6d
Maudi Rahul <maudirahul43489@gmail.com>
2026-03-16 02:51:19 +0530

    force family 4 ipv4 strictly for nodemailer

--- backend/src/utils/sendEmail.js
+++ backend/src/utils/sendEmail.js
@@ -5,6 +5,7 @@ const sendEmail = async (options) => {
     host: "smtp.gmail.com",
     port: 465,
     secure: true, 
+    family: 4,
     auth: {
       user: process.env.EMAIL_USERNAME,
       pass: process.env.EMAIL_PASSWORD,
